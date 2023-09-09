ed30bb7dfd6cff57b464ba43335beecae099d454
DorotaZielinska <122475780+DorotaZielinska@users.noreply.github.com>
2023-09-09 14:31:11 +0200

    added theme.js colors and breakpoints

---
+++ src/theme.js
@@ -0,0 +1,22 @@
+export const theme = {
+  color: {
+    white: "#FFFFFF",
+    whiteLilac: "#FBFBFE",
+    iron: "#D1D5DA",
+    mineShaft: "#252525",
+    scienceBlue: "#0366D6",
+    dodgerBlue: "#2188FF",
+    parsley: " #144620",
+    shipCove: "#6D93BE",
+    slateGray: "#6E7E91",
+    violet: "#090A33",
+    black: "#000000",
+  },
+  breakpoints: {
+    mobileVerticalMax: "575px",
+    mobileMax: "767px",
+    tabletVerticalMax: "991px",
+    tabletHorizontalMax: "1199px",
+    laptopMax: "1392px",
+  },
+};
