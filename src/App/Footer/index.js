7fd0c4186adcb3d53c33e8263a0bf5617b2a47d7
DorotaZielinska <122475780+DorotaZielinska@users.noreply.github.com>
2023-09-28 18:39:29 +0200

    added footer

---
+++ src/App/Footer/index.js
@@ -0,0 +1,37 @@
+import { email, gitHub } from "../Contact/personalContact";
+import {
+  FooterButton,
+  FooterContainer,
+  FooterHeader,
+  FooterLink,
+  FooterText,
+  IconFooterGitHub,
+  IconLinkedIn,
+  IconWrapper,
+} from "./styled";
+
+export const Footer = () => {
+  return (
+    <FooterContainer>
+      <FooterHeader>Let’s talk!</FooterHeader>
+      <FooterButton
+        href={`mailto:${email}`}
+        title={email}
+        rel="noopener noreferrer"
+      >
+        {email}
+      </FooterButton>
+      <FooterText>
+        I’m always open to new projects whenever I have the time. If you have a
+        website, dashboard or mobile app in mind and need some help to make your
+        ideas come to life, feel free to conatct me ✌
+      </FooterText>
+      <IconWrapper>
+        <FooterLink href={gitHub} rel="noopener noreferrer" target="_blank">
+          <IconFooterGitHub />
+        </FooterLink>
+        <IconLinkedIn />
+      </IconWrapper>
+    </FooterContainer>
+  );
+};
