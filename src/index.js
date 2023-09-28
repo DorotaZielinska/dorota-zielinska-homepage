a2295382da114b72c5448cc5ccfe1a9e63260e97
DorotaZielinska <122475780+DorotaZielinska@users.noreply.github.com>
2023-09-28 18:42:49 +0200

    added Provider with store

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ThemeProvider } from "styled-components";
-import { GlobalStyle } from "./GlobalStyle";
 import reportWebVitals from "./reportWebVitals";
 import App from "./App";
-import { theme } from "./theme";
+import { Provider } from "react-redux";
+import store from "./store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
+    <Provider store={store}>
       <App />
-    </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
 
