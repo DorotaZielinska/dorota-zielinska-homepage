0210142604312a321a386c1f3624081e85db9b62
DorotaZielinska <122475780+DorotaZielinska@users.noreply.github.com>
2023-10-02 21:11:55 +0200

    added light&dark theme & colors

--- src/theme.js
+++ src/theme.js
@@ -1,9 +1,10 @@
-export const theme = {
-  color: {
+ const themeColor = {
     white: "#FFFFFF",
     whiteLilac: "#FBFBFE",
     iron: "#D1D5DA",
     ironBright: "#D1D5DA4D",
+    mercury: "#E5E5E5",
+    grey: "#363636",
     mineShaft: "#252525",
     scienceBlue: "#0366D6",
     dodgerBlue: "#2188FF",
@@ -12,12 +13,104 @@ export const theme = {
     slateGray: "#6E7E91",
     violet: "#090A33",
     black: "#000000",
-  },
-  breakpoints: {
+    shadowLight: "#090a3308",
+    shadowDark: "#090a3305",
+    tropicalBlue: "#CDE0F7",
+    azure: "#35669E",
+    tundora: "#414141",
+  };
+  const breakpoints = {
     mobileVerticalMax: "575px",
     mobileMax: "767px",
     tabletVerticalMax: "991px",
     tabletHorizontalMax: "1199px",
     laptopMax: "1392px",
-  },
+  };
+  export const lightTheme = {
+primary: {
+  text: themeColor.slateGray,
+  background: themeColor.whiteLilac,
+},
+primaryBlue: {
+  text: themeColor.scienceBlue,
+  link: themeColor.scienceBlue,
+  background: themeColor.scienceBlue,
+},
+header: {
+  text: themeColor.mineShaft,
+},
+white: {
+  text: themeColor.white,
+},
+section: {
+  background: themeColor.white,
+},
+divider:{
+  background: themeColor.iron,
+},
+svg:{
+  fill: themeColor.mineShaft,
+  fillIcon: themeColor.scienceBlue,
+  hover: themeColor.scienceBlue,
+},
+svgTheme: {
+  fillBackGround: themeColor.mercury,
+  fillToggle: themeColor.slateGray,
+  fillSun: themeColor.white,
+},
+shadow: {
+  colorLight: themeColor.shadowLight,
+  colorDark: themeColor.shadowDark,
+},
+border: {
+  border: themeColor.iron,
+  borderHover: themeColor.tropicalBlue,
+},
+breakpoints,
 };
+
+export const darkTheme = {
+  primary: {
+    text: themeColor.white,
+    background: themeColor.mineShaft,
+  },
+  primaryBlue: {
+    text: themeColor.white,
+    link: themeColor.dodgerBlue,
+    background: themeColor.dodgerBlue,
+  },
+  header: {
+    text: themeColor.white,
+  },
+  white: {
+    text: themeColor.white,
+  },
+  section: {
+    background: themeColor.grey,
+  },
+  divider:{
+    background: themeColor.mercury,
+  },
+  svg:{
+    fill: themeColor.white,
+    fillIcon: themeColor.dodgerBlue,
+    hover: themeColor.dodgerBlue,
+  },
+  svgTheme: {
+    fillBackGround: themeColor.slateGray,
+    fillToggle: themeColor.white,
+    fillSun: themeColor.mineShaft,
+  },
+  shadow: {
+    colorLight: themeColor.shadowLight,
+    colorDark: themeColor.shadowDark,
+  },
+  border: {
+    border: themeColor.tundora,
+    borderHover: themeColor.azure,
+  },
+  breakpoints,
+};
+ 
+ 
+
