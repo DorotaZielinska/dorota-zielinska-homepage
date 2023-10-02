093e880b91d7f4cebaec38221015dfee391729b0
DorotaZielinska <122475780+DorotaZielinska@users.noreply.github.com>
2023-10-02 21:41:33 +0200

    added right css for ThemeButton& themeLight color

--- src/App/About/ThemeButton/styled.js
+++ src/App/About/ThemeButton/styled.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { ReactComponent as SunIcon } from "./sunIcon.svg";
 import { ReactComponent as BackgroundIcon } from "./backgroundIcon.svg";
 import { ReactComponent as ToggleIcon } from "./toggleIcon.svg";
@@ -8,26 +8,25 @@ export const Container = styled.div`
   justify-content: flex-end;
   margin: 60px auto 0;
 
-  @media(max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}) {
     align-self: flex-start;
+    margin: 21px auto 0;
   }
 `;
 
 export const Wrapper = styled.div`
-   display: inline-flex; 
+  display: inline-flex;
   justify-content: center;
   align-items: center;
-  gap: 12px; 
+  gap: 12px;
 `;
 
 export const Button = styled.button`
-background-color: transparent;
-border: none;
-/* display: flex; */
-/* align-items: center; */
-cursor: pointer;
-width: 48px;
-/* position: relative; */
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  width: 48px;
+  position: relative;
 `;
 
 export const Title = styled.p`
@@ -35,32 +34,37 @@ export const Title = styled.p`
   font-weight: 700;
   line-height: 1.3;
   text-transform: uppercase;
-  color: ${({ theme }) => theme.color.slateGray};
+  color: ${({ theme }) => theme.primary.text};
 
-  @media (max-width: ${({ theme }) => theme. breakpoints.mobileMax}) {
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}) {
     display: none;
   }
 `;
 
 export const ThemeBackgroundIcon = styled(BackgroundIcon)`
-  /* position: absolute; */
+  position: absolute;
   width: 48px;
   height: 26px;
-  display: flex;
-  
+  transform: translate(-50%, -50%);
+  fill: ${({ theme }) => theme.svgTheme.fillBackGround};
 `;
 
 export const ThemeSunIcon = styled(SunIcon)`
-  /* position: absolute; */
+  position: absolute;
   width: 14px;
   height: 14px;
-  /* transition: transform 0.3s ease; */
+  transition: transform 0.3s ease;
+  fill: ${({ theme }) => theme.svgTheme.fillSun};
+  transform: ${({ moveToRight }) =>
+    moveToRight ? "translate(-125%, -50%)" : "translate(20%, -50%)"};
 `;
 
 export const ThemeToggleIcon = styled(ToggleIcon)`
-  /* position: absolute; */
-  display: flex;
+  position: absolute;
   width: 20px;
   height: 20px;
   transition: transform 0.3s ease;
+  fill: ${({ theme }) => theme.svgTheme.fillToggle};
+  transform: ${({ moveToRight }) =>
+    moveToRight ? "translate(-100%, -50%)" : "translate(0%, -50%)"};
 `;
