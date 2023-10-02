66b9242b2c97ede3c68c5d7d37143a503caae435
DorotaZielinska <122475780+DorotaZielinska@users.noreply.github.com>
2023-10-02 21:36:57 +0200

    deleted margin

--- src/App/Portfolio/Projects/styled.js
+++ src/App/Portfolio/Projects/styled.js
@@ -4,12 +4,10 @@ export const ProjectsContainer = styled.ul`
   display: grid;
   grid-template-columns: 1fr 1fr;
   grid-gap: 32px;
-  /* margin: 0 0 24px;  */
   padding: 0;
 
   @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}) {
     grid-template-columns: 1fr;
-     grid-gap: 16px; 
-    margin: 16px;
+    grid-gap: 16px;
   }
 `;
