0cb9376f3f5b6185350c01658b5448c2ce62e84a
DorotaZielinska <122475780+DorotaZielinska@users.noreply.github.com>
2023-10-02 21:08:50 +0200

    added ThemeButton, theme mode isDarkTheme

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./GlobalStyle";
 import { Portfolio } from "./App/Portfolio";
 import { Container } from "./common/Container";
 import { About } from "./App/About";
 import { SectionSkills } from "./App/SectionSkills";
 import { skills, toLearn } from "./App/SectionSkills/skills";
 import { Footer } from "./App/Footer";
-import { ThemeProvider } from "styled-components";
-import { GlobalStyle } from "./GlobalStyle";
-import { theme } from "./theme";
+import { selectIsDarkTheme } from "./App/About/ThemeButton/themeSlice";
+import { lightTheme, darkTheme } from "./theme";
+import { ThemeButton } from "./App/About/ThemeButton";
 
-function App() {
+function App({ theme, toggleTheme}) {
+  const isDarkTheme = useSelector(selectIsDarkTheme);
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={isDarkTheme ? darkTheme : lightTheme}>
       <>
         <GlobalStyle />
         <Container>
+        <ThemeButton theme={theme} toggleTheme={toggleTheme}/> 
           <About />
           <SectionSkills 
             title="My skill set includes 🛠️" 
