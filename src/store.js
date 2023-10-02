1636aea92b9a9c2d65fa20ef022b2afbd8f3e82c
DorotaZielinska <122475780+DorotaZielinska@users.noreply.github.com>
2023-10-02 21:10:59 +0200

    added themeSlice to store

--- src/store.js
+++ src/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import ProjectSlice from "./App/Portfolio/Projects/ProjectTile/ProjectSlice";
 import createSagaMiddleware from "redux-saga";
 import { watchFetchProjects } from "./App/Portfolio/Projects/ProjectTile/ProjectSaga";
+import themeSlice from "./App/About/ThemeButton/themeSlice";
 
 const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
     reducer: {
+        theme: themeSlice,
         projects : ProjectSlice,
     },
     middleware: [ sagaMiddleware],
