59ed8e91f4a4c6e2fa90faea1000bff5ea2b14c1
DorotaZielinska <122475780+DorotaZielinska@users.noreply.github.com>
2023-10-04 18:05:35 +0200

    cleaning

--- src/App/About/Image/index.js
+++ src/App/About/Image/index.js
@@ -1,11 +1,10 @@
 import styled from "styled-components";
 
 export const Image = styled.img`
-max-width: 398px;
-border-radius: 50%;
+  max-width: 398px;
+  border-radius: 50%;
 
-@media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}) {
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}) {
     max-width: 132px;
-}
-`
-
+  }
+`;
